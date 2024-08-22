94c3d7f665c3e3d59b1d7d98b060d1bf80fda741
clarissa <clarissacavazzuti@gmail.com>
2024-08-22 13:12:34 +0200

    modelli ma da sistemare

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
+    capacity = models.IntegerField()
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='room_images/', blank=True, null=True)
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guests = models.IntegerField()
+
+class TableBooking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    guests = models.IntegerField()
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    image = models.ImageField(upload_to='review_images/', blank=True, null=True)
+
