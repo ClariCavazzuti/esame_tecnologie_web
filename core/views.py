660a899564e6cec5af3e23ae9255331747ec05b6
clarissa <clarissacavazzuti@gmail.com>
2024-08-24 18:19:25 +0200

    pagina menu

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from .models import Core
 
 def core(request):
     dati=Core.objects.all()
-    return render(request, 'core/sito-in-sviluppo.html',{'dato':dati}) 
+    return render(request, 'core/pagina-menu.html',{'dato':dati}) 
 
