6077fd398d6fd094eaf0163f1e328db572ce0822
clarissa <clarissacavazzuti@gmail.com>
2024-08-25 12:01:35 +0200

    pagina menu dinamica

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import MenuItem
 
 def core(request):
-    dati=MenuItem.objects.all()
-    return render(request, 'core/pagina-menu.html',{'dati':dati}) 
+    return render(request, 'core/sito-in-sviluppo.html') 
 
+def menu (request):
+    dati=MenuItem.objects.all()
+    return render(request, 'core/pagina-menu.html', {'dati':dati})
