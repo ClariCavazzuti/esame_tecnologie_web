21dd00ca073f2389f44dbe1280819eb7201f235c
clarissa <clarissacavazzuti@gmail.com>
2024-08-25 12:13:35 +0200

    pagina menu finita

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.core),
+    path('menu', views.menu)
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,6 @@ from .models import MenuItem
 def core(request):
     return render(request, 'core/sito-in-sviluppo.html') 
 
-def menu (request):
+def menu(request):
     dati=MenuItem.objects.all()
-    return render(request, 'core/pagina-menu.html', {'dati':dati})
+    return render(request, 'core/pagina-menu.html', {'dati':dati} )
