08854f1a63e35985e142418673a073dbe3086cc1
clarissa <clarissacavazzuti@gmail.com>
2024-08-26 11:30:27 +0200

    app autenticazione

--- progettoesame/urls.py
+++ progettoesame/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include ('core.urls')),
+    path('autenticazione/', include('autenticazione.urls')),
 ]+static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
